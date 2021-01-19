4881a18c660e467f9685ff51be85dda56e9b74e7
lcaparros <luiscaparrosruiz@gmail.com>
2021-01-19 17:26:38 +0000

    Fixed indentation error

--- packages.sh
+++ packages.sh
@@ -54,7 +54,7 @@ install_dependencies() {
     export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-    cat << EOF >> ~/.bash_profile
+    cat <<-EOF >> ~/.bash_profile
     export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
