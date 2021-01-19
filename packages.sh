a74788729d4ca3ef7851915b837e3291150ab2b2
lcaparros <luiscaparrosruiz@gmail.com>
2021-01-19 17:15:01 +0000

    Loaded nvm in bash_profile

--- packages.sh
+++ packages.sh
@@ -54,6 +54,11 @@ install_dependencies() {
     export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+    cat << EOF >> ~/.bash_profile
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+    EOF
     nvm install 14.15.4
     nvm use 14.15.4
 
